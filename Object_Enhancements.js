c2cbb6168ca2e978634a146802aa8b37608f047c
Greta Rusbarsky <Greta@Gretas-MacBook-Pro.local>
2021-04-02 09:47:22 -0500

    first commit

---
+++ Object_Enhancements.js
@@ -0,0 +1,35 @@
+//Same Key and Values
+const createInstructor = (firstName, lastName) => {
+    return {
+        firstName,
+        lastName
+      }
+    }
+
+//Computed Property Names
+let favoriteNumber = 42;
+const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+  }
+
+//Object Methods
+const instructor = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  }
+//Create animalFunction
+const createAnimal = (species, verb, noise) => {
+    return {
+        species,
+        [verb](){
+            return noise;
+        }
+    }
+
+}
